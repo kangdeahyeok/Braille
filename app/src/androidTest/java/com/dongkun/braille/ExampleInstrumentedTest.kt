42e5ea920abf71284b1702b20b851d920873f79d
nicky8209 <nicky8209@naver.com>
2021-10-30 14:02:57 +0900

    Android to Arduino

--- app/src/androidTest/java/com/dongkun/braille/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dongkun/braille/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dongkun.braille", appContext.packageName)
+        assertEquals("com.robotnmore.minismartfarm", appContext.packageName)
     }
 }
